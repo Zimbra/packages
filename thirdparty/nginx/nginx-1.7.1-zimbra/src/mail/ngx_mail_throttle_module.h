eb68b96e89cc5421d0447913e8620c7fb31e64f9
Gordon Tillman <gordyt@gmail.com>
2016-08-29 18:51:08 +0000

    ZMS-55 protocol-specific IP throttle caching
    Alter the way that the memcached keys are computed for IP throttling
    as follows.  Old method:
    
        throttle:ip=<ip-address>
    
    New method:
    
        throttle:proto=<protocol>,ip=<ip-address>
    
    Where protocol is one of the following:
    
        NGX_MAIL_POP3_PROTOCOL  "p"
        NGX_MAIL_IMAP_PROTOCOL  "i"
        NGX_MAIL_SMTP_PROTOCOL  "s"
    
    In the unlikely event we get some other protocol,
    a value of "u" (for unknown) will be used.
    
     Please enter the commit message for your changes. Lines starting

--- thirdparty/nginx/nginx-1.7.1-zimbra/src/mail/ngx_mail_throttle_module.h
+++ thirdparty/nginx/nginx-1.7.1-zimbra/src/mail/ngx_mail_throttle_module.h
@@ -58,7 +58,7 @@ struct throttle_callback_s {
 typedef struct throttle_callback_s throttle_callback_t;
 
 ngx_flag_t ngx_mail_throttle_init (ngx_mail_core_srv_conf_t *cscf);
-void ngx_mail_throttle_ip (ngx_str_t ip, throttle_callback_t *callback);
+void ngx_mail_throttle_ip (ngx_str_t ip, ngx_uint_t protocol, throttle_callback_t *callback);
 void ngx_mail_throttle_user (ngx_str_t user, throttle_callback_t *callback);
 
 extern ngx_module_t ngx_mail_throttle_module;
