85bce99e8de1730298753a694249b655273c9992
Saket Patel <saket.patel@synacor.com>
2022-04-29 01:52:07 +0530

    ZCS-11364 Initial version of circleci script - zimbra/os-requirements - add dependency of libgmp10, libperl5.26 for ubuntu 18 to fix compilation - thirdparty/openssl and thirdparty/libxml2 - add dependency of zlib1g-dev to fix compilation - Upgrade freetype to 2.12.0, older version 2.6.3 had different path to download so instead updated to latest

---
+++ scripts/build-packages.sh
@@ -0,0 +1,46 @@
+#/bin/bash
+set -e
+
+##
+# Compile function which will call make function in individual packages
+##
+function compile() {
+    local pkg="${1}"
+    local pkg_name=$(echo "${pkg}" | cut -d '/' -f 2)
+
+    cd "${pkg}" || exit 1
+
+    # Generate packages
+    make
+
+    # Zip & Copy build artifacts to different location so it can be picked up by circleci
+    tar -czf "../../dist-packages/${pkg_name}.tar.gz" -C build/UBUNTU18_64/ .
+
+    cd ../../ || exit 1
+}
+
+##
+# Main function which gets list of packages which are updated compared to develop branch
+# and calls compile function to compile the package
+##
+function main() {
+    local modified_only="${1:-false}"
+
+    # Remove folder if already present
+    if [ -d "dist-packages" ]; then
+        rm -Rf dist-packages;
+    fi
+
+    # Create destination folder
+    mkdir dist-packages
+
+    while IFS= read -r pkg
+    do
+        # @TODO check if package has modifications
+        echo -e "################## Compiling \"${pkg}\" ..."
+        compile "${pkg}"
+    done < "build-order"
+}
+
+main "$@"
+
